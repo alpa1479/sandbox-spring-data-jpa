2d34c53d3a2b7c624246375de8b60c2f28232e2e
alpa1479 <alpa1479@gmail.com>
2023-12-30 15:55:53 +0400

    [main] - Added run configurations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,6 @@ pluginManagement {
 include("examples:relationships:bidirectional:many-to-many")
 include("examples:relationships:bidirectional:one-to-many")
 include("examples:relationships:bidirectional:one-to-one")
-
 include("examples:relationships:unidirectional:many-to-many")
 include("examples:relationships:unidirectional:one-to-many")
 include("examples:relationships:unidirectional:one-to-one")
